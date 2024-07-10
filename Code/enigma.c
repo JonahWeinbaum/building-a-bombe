ef99e48300bc551fd20dcf5dcc039b74bcff607b
JonahWeinbaum <Jonah.r.weinbaum.gr@dartmouth.edu>
2024-07-10 16:36:22 -0400

    My god I hate Ringstellung

--- Code/enigma.c
+++ Code/enigma.c
@@ -34,8 +34,8 @@ typedef struct parameters
 } t_parameters;
 
 t_parameters key_sheet = {
-    {1, 2, 5}, 
-    {1, 1, 1},
+    {3, 2, 1}, 
+    {1, 1, 2},
     "AAA",
     ""
     //"POMLIUKJNHYTGBVFREDC"
@@ -97,82 +97,124 @@ char P_inv(char in, int deg, bool is_logging) {
 }
 
 /*First Rotor*/
-char N(char in, int shift, bool is_logging) {
+char N(char in, bool is_logging) {
+
+    //Ceasar Shift For Rotation (Considering Ringstellung)
+    char in_shift = P(in, enigma_state.shifts[2]-(key_sheet.ringstellung[2]-1), false);
+
+    //Map Letter
     char *rotor_mapping = rotor[(size_t)(key_sheet.walzenlage[2] - 1)];
-    char rm = rotor_mapping[(in - 'A' + shift) % sizeof(alphabet)];
-    if (is_logging) {printf("--W: %c->%c\n", in, P(rm, key_sheet.ringstellung[2] - 1, false));}
-    return P(rm, key_sheet.ringstellung[2] - 1, false);
+    char out = rotor_mapping[(in_shift - 'A') % sizeof(alphabet)];
+
+    //Reverse Ceasar Shift (Considering Ringstellung)
+    char out_shift = P_inv(out, enigma_state.shifts[2]-(key_sheet.ringstellung[2]-1), false);
+
+    //Log and Output
+    if (is_logging) {printf("--W: %c->%c\n", in, out_shift);}
+    return out_shift;
 }
 
-char N_inv(char in, int shift, bool is_logging) {
+char N_inv(char in, bool is_logging) {
     //Setup letter map
     char *rotor_mapping = rotor[(size_t)(key_sheet.walzenlage[2] - 1)];
     char letter_map[26];
     for (int i = 0; i < 26; i++) {
-        letter_map[i] = N(i + 'A', shift, false);
+        letter_map[i] = N(i + 'A', false);
     }
 
+    //Find Letter in Map
     for (int i = 0; i < 26; i++) {
         if (letter_map[i] == in) {
+
+            //Log and Output
             if (is_logging) {printf("--W: %c->%c\n", in, i + 'A');}
             return i + 'A';
         }
     }
-    return in;
+    
+    //Letter Map Failure
+    return -1;
 }
 
 /*Second Rotor*/
-char M(char in, int shift, bool is_logging) {
-    //Shift input by inverse of prior rotor shift
-    char in_new = P(in, (26 - enigma_state.shifts[2] % sizeof(alphabet)), false);
+char M(char in, bool is_logging) {
+
+    //Ceasar Shift For Rotation (Considering Ringstellung)
+    char in_shift = P(in, enigma_state.shifts[1]-(key_sheet.ringstellung[1]-1), false);
+
+    //Map Letter
     char *rotor_mapping = rotor[(size_t)(key_sheet.walzenlage[1] - 1)];
-    char rm = rotor_mapping[(in_new - 'A' + shift) % sizeof(alphabet)];
-    if (is_logging) {printf("-W-: %c->%c\n", in, P(rm, key_sheet.ringstellung[1] - 1, false));}
-    return P(rm, key_sheet.ringstellung[1] - 1, false);
+    char out = rotor_mapping[(in_shift - 'A') % sizeof(alphabet)];
+
+    //Reverse Ceasar Shift (Considering Ringstellung)
+    char out_shift = P_inv(out, enigma_state.shifts[1]-(key_sheet.ringstellung[1]-1), false);
+
+    //Log and Output
+    if (is_logging) {printf("-W-: %c->%c\n", in, out_shift);}
+    return out_shift;
 }
 
-char M_inv(char in, int shift, bool is_logging) {
+char M_inv(char in, bool is_logging) {
     //Setup letter map
     char *rotor_mapping = rotor[(size_t)(key_sheet.walzenlage[1] - 1)];
     char letter_map[26];
     for (int i = 0; i < 26; i++) {
-        letter_map[i] = M(i + 'A', shift, false);
+        letter_map[i] = M(i + 'A', false);
     }
 
+    //Find Letter in Map
     for (int i = 0; i < 26; i++) {
         if (letter_map[i] == in) {
+            
+            //Log and Output
             if (is_logging) {printf("-W-: %c->%c\n", in, i + 'A');}
             return i + 'A';
         }
     }
-    return in;
+    
+    //Letter Map Failure
+    return -1;
 }
 
 /*Third Rotor*/
-char L(char in, int shift, bool is_logging) {
-    char in_new = P(in, (26 - enigma_state.shifts[1] % sizeof(alphabet)), false);
+char L(char in, bool is_logging) {
+    //Ceasar Shift For Rotation (Considering Ringstellung)
+    char in_shift = P(in, enigma_state.shifts[0]-(key_sheet.ringstellung[0]-1), false);
+
+    //Map Letter
     char *rotor_mapping = rotor[(size_t)(key_sheet.walzenlage[0] - 1)];
-    char rm = rotor_mapping[(in_new - 'A' + shift) % sizeof(alphabet)];
-    if (is_logging) {printf("W--: %c->%c\n", in, P(rm, key_sheet.ringstellung[0] - 1, false));}
-    return P(rm, key_sheet.ringstellung[0] - 1, false);
+    char out = rotor_mapping[(in_shift - 'A') % sizeof(alphabet)];
+
+    //Reverse Ceasar Shift (Considering Ringstellung)
+    char out_shift = P_inv(out, enigma_state.shifts[0]-(key_sheet.ringstellung[0]-1), false);
+
+    //Log and Output
+    if (is_logging) {printf("W--: %c->%c\n", in, out_shift);}
+    return out_shift;
 }
 
-char L_inv(char in, int shift, bool is_logging) {
+char L_inv(char in, bool is_logging) {
     //Setup letter map
     char *rotor_mapping = rotor[(size_t)(key_sheet.walzenlage[0] - 1)];
     char letter_map[26];
     for (int i = 0; i < 26; i++) {
-        letter_map[i] = L(i + 'A', shift, false);
+        letter_map[i] = L(i + 'A', false);
     }
 
+    //Find Letter in Map
     for (int i = 0; i < 26; i++) {
         if (letter_map[i] == in) {
+            
+            //Log and Output
             if (is_logging) {printf("W--: %c->%c\n", in, i + 'A');}
             return i + 'A';
         }
     }
-    return in;
+    
+    //Letter Map Failure
+    return -1;
 }
+
 /*Reflector*/
 char R(char in, bool is_logging) {
     char *reflector_mapping = reflector[0];
@@ -185,9 +227,10 @@ char R(char in, bool is_logging) {
 /*Standard Enigma Encryption*/
 void enigma_reset() {
     //Check Notch Flags
-    enigma_state.notches_engaged[1] = ((turnover[key_sheet.walzenlage[1]-1] - 'A' + key_sheet.ringstellung[1] - 1) % sizeof(alphabet) + 'A'  == enigma_state.window[2]);
-    enigma_state.notches_engaged[0] = ((turnover[key_sheet.walzenlage[0]-1] - 'A' + key_sheet.ringstellung[0] - 1) % sizeof(alphabet) + 'A' == enigma_state.window[1]);
-    
+    enigma_state.notches_engaged[1] = (turnover[key_sheet.walzenlage[2]-1]   == enigma_state.window[2]);
+    enigma_state.notches_engaged[0] = (turnover[key_sheet.walzenlage[1]-1]   == enigma_state.window[1]);
+
+    //Initialize
     for (int i = 0; i < 3; i++) {
         enigma_state.shifts[i] = 0;
         enigma_state.window[i] = key_sheet.spruchschlusse[i];
@@ -215,9 +258,8 @@ char enigma_encrypt(char in) {
                      , enigma_state.window[2]); }
 
     //Check Notch Flags
-    enigma_state.notches_engaged[1] = ((turnover[key_sheet.walzenlage[1]-1] - 'A' + key_sheet.ringstellung[1] - 1) % sizeof(alphabet) + 'A'  == enigma_state.window[2]);
-    enigma_state.notches_engaged[0] = ((turnover[key_sheet.walzenlage[0]-1] - 'A' + key_sheet.ringstellung[0] - 1) % sizeof(alphabet) + 'A' == enigma_state.window[1]);
-
+    enigma_state.notches_engaged[1] = (turnover[key_sheet.walzenlage[2]-1]   == enigma_state.window[2]);
+    enigma_state.notches_engaged[0] = (turnover[key_sheet.walzenlage[1]-1]   == enigma_state.window[1]);
     //Encrypt
     return  S_inv(
                     N_inv(
@@ -228,23 +270,66 @@ char enigma_encrypt(char in) {
                                         M(
                                             N(
                                                 S(in, logging)
-                                            , enigma_state.shifts[2], logging)
-                                        , enigma_state.shifts[1], logging)
-                                    , enigma_state.shifts[0], logging)
+                                            , logging)
+                                        , logging)
+                                    , logging)
                                 , logging
                                 )
-                            , enigma_state.shifts[0], logging)
-                        , enigma_state.shifts[1], logging)
-                    , enigma_state.shifts[2], logging)
+                            , logging)
+                        , logging)
+                    , logging)
                 , logging
                 );
+}
+
+void displayCeasar(char (*mappingFunction)(char, int, bool), int deg, bool is_logging) {
+    // Print the top row (A to Z)
+    for (char c = 'A'; c <= 'Z'; c++) {
+        printf("%c ", c);
+    }
+    printf("\n");
+
+    // Print the mapping arrows
+    for (char c = 'A'; c <= 'Z'; c++) {
+        printf("| ");
+    }
+    printf("\n");
+
+    // Print the bottom row (mapped characters)
+    for (char c = 'A'; c <= 'Z'; c++) {
+        printf("%c ", mappingFunction(c, deg, is_logging));
+    }
+    printf("\n");
+}
+
+void displayRotor(char (*mappingFunction)(char, bool), bool is_logging) {
+    // Print the top row (A to Z)
+    for (char c = 'A'; c <= 'Z'; c++) {
+        printf("%c ", c);
+    }
+    printf("\n");
+
+    // Print the mapping arrows
+    for (char c = 'A'; c <= 'Z'; c++) {
+        printf("| ");
+    }
+    printf("\n");
+
+    // Print the bottom row (mapped characters)
+    for (char c = 'A'; c <= 'Z'; c++) {
+        printf("%c ", mappingFunction(c, is_logging));
+    }
+    printf("\n");
 }
 
 
 int main() {
     enigma_reset();
-    for (int i = 0; i < 4; i ++) {
-        printf("Lamp: %c\n", enigma_encrypt('A'));
+    // key_sheet.ringstellung[0] = 2;
+    // displayRotor(N, logging);
+    
+    for (int i = 0; i < 30; i ++) {
+        printf("%c", enigma_encrypt('A'));
     }
     return 0;
 }

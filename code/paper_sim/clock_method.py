56718e4631c8223e8c462a6bb2877d3a912d30ed
JonahWeinbaum <Jonah.r.weinbaum.gr@dartmouth.edu>
2025-07-12 21:56:56 -0400

    Updated clock method code

--- code/paper_sim/clock_method.py
+++ code/paper_sim/clock_method.py
@@ -1,21 +1,76 @@
-from pyenigma import *
+from pyenigma import rotor, enigma
 import random
 import nltk
 nltk.download('words')
 from nltk.corpus import words
+import string
 
+letters = ''.join(random.choices(string.ascii_uppercase, k=4))
 word_list = words.words()
 
-def random_sentence(n=10):
-    return ''.join(random.choice(word_list) for _ in range(n))
+KEY1 = letters[0] + letters[1] + letters[2]
+KEY2 = letters[0] + letters[1] + letters[3]
 
-ROTORS = [
+def random_sentence(n=10):
+    return ''.join(random.choice(word_list).upper() for _ in range(n))
+
+ROTORS_SET = [
     rotor.ROTOR_I,
     rotor.ROTOR_II,
     rotor.ROTOR_III,
 ]
 
-# e = enigma.Enigma(reflector, rotors[0], rotors[1], rotors[2], key, plugs="")
-# e.encipher("a" * offset)
+rotors = random.sample(ROTORS_SET, k=3)
 
-print(random_sentence(n=15))
+e1 = enigma.Enigma(rotor.ROTOR_Reflector_B, rotors[0], rotors[1], rotors[2], KEY1, plugs="")
+e2 = enigma.Enigma(rotor.ROTOR_Reflector_B, rotors[0], rotors[1], rotors[2], KEY2, plugs="")
+
+text1 = random_sentence(n=10)
+text2 = random_sentence(n=10)
+etext1 = e1.encipher(text1)
+etext2 = e2.encipher(text2)
+
+print()
+print(f"Rotor used -> {rotors[2].name}")
+print("-"*(abs((ord(KEY2[-1]) - ord(KEY1[-1])) % 26) + len(etext2) + 5))
+print(f"{KEY1}: {etext1}")
+print(f"{KEY2}: " + " "*abs((ord(KEY2[-1]) - ord(KEY1[-1])) % 26) + f"{etext2}")
+print(f"     " + " "*abs((ord(KEY2[-1]) - ord(KEY1[-1])) % 26), end="")
+overlap1 = min(len(etext2), len(etext1) - abs((ord(KEY2[-1]) - ord(KEY1[-1])) % 26))
+coincidences1 = 0
+for i in range(overlap1):
+    if etext2[i] == etext1[i + abs((ord(KEY2[-1]) - ord(KEY1[-1])) % 26)]:
+        print("*", end="")
+        coincidences1 += 1
+    else:
+        print(" ", end="")
+print()
+print("-"*(abs((ord(KEY2[-1]) - ord(KEY1[-1])) % 26) + len(etext2) + 5))
+if coincidences1 / float(overlap1) > (1/26) + 0.04:
+    print(f"No turnover between {KEY1[2]} and {KEY2[2]}")
+else:
+    print(f"Turnover between {KEY1[2]} and {KEY2[2]}")
+print("-"*(abs((ord(KEY2[-1]) - ord(KEY1[-1])) % 26) + len(etext2) + 5))
+
+print()
+
+print("-"*(abs((ord(KEY1[-1]) - ord(KEY2[-1])) % 26) + len(etext1) + 5))
+print(f"{KEY2}: {etext2}")
+print(f"{KEY1}: " + " "*abs((ord(KEY1[-1]) - ord(KEY2[-1])) % 26) + f"{etext1}")
+print(f"     " + " "*abs((ord(KEY1[-1]) - ord(KEY2[-1])) % 26), end="")
+overlap2 = min(len(etext1), len(etext2) - abs((ord(KEY1[-1]) - ord(KEY2[-1])) % 26))
+coincidences2 = 0
+for i in range(overlap2):
+    if etext1[i] == etext2[i + abs((ord(KEY1[-1]) - ord(KEY2[-1])) % 26)]:
+        print("*", end="")
+        coincidences2 += 1
+    else:
+        print(" ", end="")
+print()
+if coincidences2 / float(overlap2) > (1/26) + 0.04:
+    print(f"No turnover between {KEY2[2]} and {KEY1[2]}")
+else:
+    print(f"Turnover between {KEY2[2]} and {KEY1[2]}")
+
+print("-"*(abs((ord(KEY1[-1]) - ord(KEY2[-1])) % 26) + len(etext1) + 5))
+
